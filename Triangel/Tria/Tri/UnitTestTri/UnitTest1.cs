ffefe8d6037535f38023298dbbee899a0148f8d0
sslnustudent <ss222tb@student.lnu.se>
2013-11-26 17:51:45 +0100

    Skrivit och kört enhetstesterna

--- Triangel/Tria/Tri/UnitTestTri/UnitTest1.cs
+++ Triangel/Tria/Tri/UnitTestTri/UnitTest1.cs
@@ -49,15 +49,35 @@ namespace UnitTestTri
         [TestMethod]
         public void ConstTest2()
         {
-            double[] arr = new double[3];
-            arr[0] = 3.0;
-            arr[1] = 3.0;
-            arr[2] = 5.0;
-            Triangle tritest = new Triangle(arr);
-            Assert.IsTrue(tritest.isIsosceles());
-            Assert.IsFalse(tritest.isScalene());
-            Assert.IsFalse(tritest.isEquilateral());
+            Point a;
+            Point b;
+            Point c;
+            a.x = 1;
+            a.y = 1;
+            b.x = 2;
+            b.y = 1;
+            c.x = 1;
+            c.y = 2;
+            Triangle tritest = new Triangle(a, b, c);
+           // Assert.IsTrue(tritest.isIsosceles());
+            // Assert.IsTrue(tritest.isScalene());
+            Assert.IsTrue(tritest.isEquilateral());
         }
 
+        [TestMethod]
+        public void ConstTest3()
+        {
+            Point[] s = new Point[3];
+            s[0].x = 1;
+            s[0].y = 1;
+            s[1].x = 2;
+            s[1].y = 1;
+            s[2].x = 1;
+            s[2].y = 2;
+            Triangle tritest = new Triangle(s);
+            // Assert.IsTrue(tritest.isIsosceles());
+            // Assert.IsTrue(tritest.isScalene());
+            Assert.IsTrue(tritest.isEquilateral());
+        }
     }
 }
